109f32dfb55cfbda977ae1623cfea87fa048eba0
77r77ung <2625k@naver.com>
2022-01-10 17:58:27 +0900

    2022.01.10 16:45 세미나 알고리즘 테스트
    TODO 3번 관련 문제 보충

--- week1/assignment_4.py
+++ week1/assignment_4.py
@@ -2,6 +2,14 @@
 4. 알파벳 소문자로 이루어진 N개의 단어가 들어오면 아래와 같은 조건에 따라 정렬하는 프로그램
     a) 길이가 짧은 것부터
     b) 길이가 같으면 사전 순으로
+    TODO 조건)문자열의 길이는 50을 넘지 않는다.
+    for j in arr:
+        if int(len(arr[j])) > 50:
+            del(arr[i])
+    TODO 조건)대문자가 입력되었을 때
+    TODO 조건)단어의 개수에 대한 조건 (1 <= N <= 20000)
+
+    arr.sort(): 알파벳 순서로 정렬
 '''
 arr = []
 
@@ -14,12 +22,29 @@ new.sort(key = lambda x : (len(x), x))
 print(new)
 
 '''
-TODO 조건)문자열의 길이는 50을 넘지 않는다.
-for j in arr:
-    if int(len(arr[j])) > 50:
-        del(arr[i])
-TODO 조건)대문자가 입력되었을 때
-TODO 조건)단어의 개수에 대한 조건 (1 <= N <= 20000)
+import sys
 
-arr.sort(): 알파벳 순서로 정렬
+# sys.stdin.readline(): input보다 입력 시간을 줄여줌
+count = int(sys.stdin.readline())
+
+words = list()
+
+for _ in range(count):
+    words.append(input())
+
+# set(배열명): 중복 제거, 순서가 없어짐
+# list(set(배열명)): 중복을 제거해주고 그 요소들을 list로 다시 만들어줌
+words = list(set(words))
+
+# 사전 순으로 정렬 (sort의 default: 사전순)
+words.sort()
+
+# 길이 순으로 다시 정렬
+words.sort(key = len)
+
+for word in words:
+    print(word)
+
+!! 배열명.sort(key = lambda x : (len(x), x))
+lamda를 쓰는 것과 그냥 key = len으로 사용하는 것의 차이점은 무엇일까?
 '''
--- week1/assignment_5.py
+++ week1/assignment_5.py
@@ -1,6 +1,10 @@
 '''
 5. 2차원 평면 위의 점 N개가 주어진다. 좌표를 x좌표가 증가하는 순으로, x좌표가 같으면 y 좌표가 증가하는 순서로 정렬한 다음 출력하는 프로그램
     -> 2차원 리스트 / input().split()
+
+    TODO 조건)점의 개수 조건(1 <= N <= 100000)
+    TODO 조건)점의 위치 조건(-100000 <= x, y <= 100,000)
+    TODO 조건) 위치가 같은 두 점은 존재 안 함
 '''
 arr = []
 n = int(input("점의 개수: "))
@@ -17,7 +21,24 @@ for x, y in arr:
     print(x, y)
 
 '''
-TODO 조건)점의 개수 조건(1 <= N <= 100000)
-TODO 조건)점의 위치 조건(-100000 <= x, y <= 100,000)
-TODO 조건) 위치가 같은 두 점은 존재 안 함
+count = int(input())
+
+# 위치 배열 선언
+position = list()
+
+for index in range(count):
+    # "3, 4" -> ["3", "4"] -> [3, 4] == 문자열 -> 문자열 배열 -> 숫자 배열
+    x, y = map(int, input().split())
+
+    # 위치를 배열로 저장 position = [[3, 4]]
+    position.append((x, y))
+
+# x좌표, y좌표 정렬
+# num == (x, y)
+# 1번째 조건: x를 기준으로 정렬(num[0])
+# 2번째 조건: y를 기준으로 정렬(num[1])
+position.sort(key = lambda num: (num[0], num[1]))
+
+for num in postion:
+    print(num[0], num[1])
 '''
